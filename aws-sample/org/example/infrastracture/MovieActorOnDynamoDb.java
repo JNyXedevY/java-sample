6476caf13aedeeff4cf16ea35dc29e8d3f9b7743
JNyXedevY <111258155+JNyXedevY@users.noreply.github.com>
2024-09-01 13:12:48 +0900

    Add BatchWrite BatchRead EnhancedClient

--- aws-sample/org/example/infrastracture/MovieActorOnDynamoDb.java
+++ aws-sample/org/example/infrastracture/MovieActorOnDynamoDb.java
@@ -4,7 +4,10 @@ import org.example.domain.MovieActor;
 import software.amazon.awssdk.core.internal.waiters.ResponseOrException;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
-import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.model.BatchGetResultPageIterable;
+import software.amazon.awssdk.enhanced.dynamodb.model.BatchWriteResult;
+import software.amazon.awssdk.enhanced.dynamodb.model.ReadBatch;
+import software.amazon.awssdk.enhanced.dynamodb.model.WriteBatch;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.DescribeTableResponse;
 import software.amazon.awssdk.services.dynamodb.waiters.DynamoDbWaiter;
@@ -12,24 +15,26 @@ import software.amazon.awssdk.services.dynamodb.waiters.DynamoDbWaiter;
 import java.util.List;
 
 public class MovieActorOnDynamoDb {
-    private final String tableName = "MovieActor";
     private final DynamoDbClient dbClient;
+    private final DynamoDbEnhancedClient enhancedClient;
     private final DynamoDbTable<MovieActor> table;
 
     public MovieActorOnDynamoDb(Builder b){
-        this.dbClient = b.client;
-        this.table = DynamoDbEnhancedClient.builder()
-                .dynamoDbClient(b.client)
-                .build()
-                .table(tableName, TableSchema.fromImmutableClass(MovieActor.class));
+        this.dbClient = b.dbClient;
+        this.enhancedClient = b.enhancedClient;
+        this.table = b.table;
     }
 
     public static Builder builder(){ return new Builder(); }
 
     public static final class Builder {
-        private DynamoDbClient client;
+        private DynamoDbClient dbClient;
+        private DynamoDbEnhancedClient enhancedClient;
+        private DynamoDbTable<MovieActor> table;
 
-        public Builder client(DynamoDbClient client){ this.client = client; return this;}
+        public Builder dbClient(DynamoDbClient dbClient){ this.dbClient= dbClient; return this;}
+        public Builder enhancedClient(DynamoDbEnhancedClient enhancedClient){ this.enhancedClient = enhancedClient; return this; }
+        public Builder table(DynamoDbTable<MovieActor> table){ this.table = table; return this; }
 
         public MovieActorOnDynamoDb build(){ return new MovieActorOnDynamoDb(this); }
     }
@@ -38,7 +43,7 @@ public class MovieActorOnDynamoDb {
         table.createTable();
         try(DynamoDbWaiter waiter = DynamoDbWaiter.builder().client(dbClient).build()){
             ResponseOrException<DescribeTableResponse> response = waiter
-                    .waitUntilTableExists(builder -> builder.tableName(tableName).build())
+                    .waitUntilTableExists(builder -> builder.tableName("MovieActor").build())
                     .matched();
 
             response.response().orElseThrow();
@@ -51,6 +56,35 @@ public class MovieActorOnDynamoDb {
 
     public void putItem(MovieActor item){
         table.putItem(item);
+    }
+
+    public List<MovieActor> batchGetItem(String movieName, String actorName){
+        ReadBatch readBatch = ReadBatch.builder(MovieActor.class)
+                .mappedTableResource(table)
+                .addGetItem(builder -> builder
+                        .key(key -> key
+                                .partitionValue(movieName)
+                                .sortValue(actorName)
+                        )
+                )
+                .build();
+        BatchGetResultPageIterable resultPages = enhancedClient
+                .batchGetItem(builder -> builder.readBatches(readBatch));
+        return resultPages.resultsForTable(table).stream().toList();
+    }
+
+    public void batchWriteItem(List<MovieActor> items){
+        List<WriteBatch> batches = items
+                .stream()
+                .map(v -> WriteBatch.builder(MovieActor.class)
+                        .mappedTableResource(table)
+                        .addPutItem(v)
+                        .build()
+                ).toList();
+        BatchWriteResult result = enhancedClient.batchWriteItem(builder -> builder.writeBatches(batches));
+        if (!result.unprocessedPutItemsForTable(table).isEmpty()) {
+            System.out.println(result.unprocessedPutItemsForTable(table).stream().map(MovieActor::toString));
+        }
     }
 
 }
